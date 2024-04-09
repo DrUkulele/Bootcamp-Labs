2be65a023bce6328de5bf0337746c1ff74602d5e
DrUkulele <tlpriest@ymail.com>
2024-04-09 15:11:22 -0500

    Third commit yes.

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is my leet new app!");
         System.out.println("It will do something I think?");
+
+        System.out.println("It definitely does something");
     }
 }
