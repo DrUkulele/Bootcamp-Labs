d90f233355d9f466ef61e9e8a6418d097ff096b5
DrUkulele <tlpriest@ymail.com>
2024-04-09 15:44:33 -0500

    full program 1 bug with asking for user input on operator

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -4,16 +4,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //variables
         float firstNumber;
         float secondNumber;
-        System.out.println("This is a basic calculator that can do basic math with two numbers.\n");
-        System.out.println("Please enter the first number:");
+        String calculations;
 
+        //user input scanner
         Scanner scanner = new Scanner(System.in);
+
+        //what project does
+        System.out.println("This is a basic calculator that can do basic math with two numbers.\n");
+
+        //get numbers
+        System.out.println("Please enter the first number:");
         firstNumber = scanner.nextFloat();
 
         System.out.println("Please enter the second number:");
         secondNumber = scanner.nextFloat();
+
+        //ask user for math operation they want ot do
+        System.out.println("Please enter one if the following Possible calculations\n");
+        System.out.println("Add or +.\n");
+        System.out.println("Subtract or -.\n");
+        System.out.println("Multiply or *.\n");
+        System.out.println("Divided or /.\n");
+
+        calculations = scanner.nextLine();
+
+        //do calculation
+        if (calculations.equalsIgnoreCase("Add") || calculations.equals("+")) {
+            System.out.println(firstNumber + secondNumber);
+        } else if (calculations.equalsIgnoreCase("Subtract") || calculations.equals("-")) {
+            System.out.println(firstNumber - secondNumber);
+
+        } else if (calculations.equalsIgnoreCase("Multiply") || calculations.equals("*")) {
+            System.out.println(firstNumber * secondNumber);
+
+        } else
+            System.out.println(firstNumber / secondNumber);
 
     }
 }
