703e0439f21483480a6ef19be5c130a0e158c405
DrUkulele <tlpriest@ymail.com>
2024-04-11 11:54:40 -0500

    moved methods to new class and added print statments

--- RentalCarCalculator/src/main/java/org/example/Main.java
+++ RentalCarCalculator/src/main/java/org/example/Main.java
@@ -9,214 +9,80 @@ public class Main {
     public static void main(String[] args) {
         //variables
         String pickUpDate;
-        int daysToRent;
-        int currentAge;
-        boolean tollTag;
-        boolean gps;
-        boolean roadSide;
+        double startingCharge = 29.99;
+        double daysToRent; //= 10;
+        double options = 0;
+        double surcharge = 0;
+        boolean underAge; //= true;
+        boolean tollTag; //= true;
+        boolean gps; //= true;
+        boolean roadSide; //= true;
         Scanner scanner = new Scanner(System.in);
 
         //Introduction
         System.out.println("Hello and welcome to xyz car rental company rental site.");
+//        double total = startingCharge * daysToRent;
+//        System.out.printf("Your total is: %f\n", total);
 
-        pickUpDate = pickupdate();
-        daysToRent = daystorent();
-        tollTag = tolltag();
-        gps = gps();
-        roadSide = roadside();
-        currentAge = age();
-    }
+        pickUpDate = Methods.pickupdate();
+//        //System.out.printf("This method returns %s\n", pickUpDate);
+        daysToRent = Methods.daystorent();
+//        //System.out.printf("This method returns %f\n", daysToRent);
+        tollTag = Methods.tolltag();
+//        //System.out.printf("This method returns %B\n", tollTag);
+        gps = Methods.gps();
+//        //System.out.printf("This method returns %B\n", gps);
+        roadSide = Methods.roadside();
+        //System.out.printf("This method returns %B\n", roadSide);
+        underAge = Methods.age();
+        //System.out.printf("This method returns %d\n", currentAge);
+
+        double total = startingCharge * daysToRent;
+        double basicRental = startingCharge * daysToRent;
 
 
-    public static String pickupdate() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "n";
-        String pickUpDate = "";
-
-        while (confirm.equalsIgnoreCase("n") || confirm.equalsIgnoreCase("no")) {
-            //user enters pickup date
-            System.out.println("Please enter the date you want to pick up the rental care in mm-dd-yyyy format.");
-            pickUpDate = scanner.nextLine();
-            System.out.printf("The pickup date you entered is %s. Is that the correct date? (y/n)\n", pickUpDate);
-            confirm = scanner.nextLine();
-
+        if (underAge = true) {
+            surcharge = (total * .30);
+            total = surcharge + total;
+            //System.out.println(total);
         }
-        return pickUpDate;
-    }
 
-    public static int daystorent() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "n";
-        int daysToRent = 0;
-
-        while (confirm.equalsIgnoreCase("n")) {
-            //user enters pickup date
-            System.out.println("Please enter how many days you want to rent the car.");
-            daysToRent = scanner.nextInt();
-            scanner.nextLine();
-            System.out.printf("You want to rent the car for %d. (y/n)\n", daysToRent);
-            confirm = scanner.nextLine();
-
+        if (tollTag == true) {
+            options = (3.95 * daysToRent) + options;
+            //total = options + total;
+            //System.out.println(total);
         }
-        return daysToRent;
-    }
-
-    public static boolean tolltag() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "";
-        boolean tollTag = false;
-        boolean userInput = true;
-
-        while (userInput == true) {
-            //user enters pickup date
-            System.out.println("Would you like to add an Electronic Toll Tag for $3.95 a day?(y/n) ");
-            confirm = scanner.nextLine();
-            switch (confirm.toLowerCase()) {
-                case "y":
-                    System.out.println("You have chosen to add an electronic Toll Tag for $3.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have added the Toll Tag.");
-                            tollTag = true;
-                            userInput = false;
-                            break;
-                        case "n":
-                            tolltag();
-                            break;
-
-                    }
-                    break;
-
-                case "n":
-                    System.out.println("You have chosen not to add an electronic Toll Tag for $3.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have not added the Toll Tag.");
-                            userInput = false;
-                            break;
-                        case "n":
-                            tolltag();
-                            break;
-                    }
-                    break;
-            }
-
+        if (gps == true) {
+            options = (2.95 * daysToRent) + options;
+            //total = options + total;
+            //System.out.println(total);
         }
-        return tollTag;
-    }
-
-    public static boolean gps() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "";
-        boolean gps = false;
-        boolean userInput = true;
-
-        while (userInput == true) {
-            //user enters pickup date
-            System.out.println("Would you like to add GPS for $2.95 a day?(y/n) ");
-            confirm = scanner.nextLine();
-            switch (confirm.toLowerCase()) {
-                case "y":
-                    System.out.println("You have chosen to add GPS for $2.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have added the GPS.");
-                            gps = true;
-                            userInput = false;
-                            break;
-                        case "n":
-                            gps();
-                            break;
-
-                    }
-                    break;
-
-                case "n":
-                    System.out.println("You have chosen not to add GPS for $2.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have not added the GPS.");
-                            userInput = false;
-                            break;
-                        case "n":
-                            gps();
-                            break;
-                    }
-                    break;
-            }
-
+        if (roadSide == true) {
+            options = (3.95 * daysToRent) + options;
+            //total = options + total;
+            //System.out.println(total);
         }
-        return gps;
-    }
+        total = options + total;
 
-    public static boolean roadside() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "";
-        boolean roadSide = false;
-        boolean userInput = true;
-
-        while (userInput == true) {
-            //user enters pickup date
-            System.out.println("Would you like to add Roadside Assistance for $3.95 a day?(y/n) ");
-            confirm = scanner.nextLine();
-            switch (confirm.toLowerCase()) {
-                case "y":
-                    System.out.println("You have chosen to add Roadside Assistance for $3.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have added Roadside Assistance.");
-                            roadSide = true;
-                            userInput = false;
-                            break;
-                        case "n":
-                            roadside();
-                            break;
-
-                    }
-                    break;
-
-                case "n":
-                    System.out.println("You have chosen not to add Roadside Assistance for $3.95 a day.\n Is that correct?(y/n)");
-                    confirm = scanner.nextLine();
-                    switch (confirm.toLowerCase()) {
-                        case "y":
-                            System.out.println("You have not added Roadside Assistance.");
-                            userInput = false;
-                            break;
-                        case "n":
-                            roadside();
-                            break;
-                    }
-                    break;
-            }
-
+        System.out.printf("Your base rental charge is: %.2f\n", basicRental);
+        System.out.printf("Your optinal additions charge is: %.2f\n", options);
+        if (underAge == true) {
+            System.out.printf("You are under the age of 25. There is a surcharge of %.2f\n", surcharge);
         }
-        return roadSide;
+        System.out.printf("Your total is: %.2f", total);
 
+
+        //Do not comment out Main method }
     }
+    //Do not comment out mainMethod }
 
-    public static int age() {
-        Scanner scanner = new Scanner(System.in);
-        String confirm = "n";
-        int age = 0;
 
-        while (confirm.equalsIgnoreCase("n")) {
-            //user enters pickup date
-            System.out.println("Please enter how old you are in years.");
-            age = scanner.nextInt();
-            scanner.nextLine();
-            System.out.printf("You are %d years old? (y/n)\n", age);
-            confirm = scanner.nextLine();
-
-        }
-        return age;
-    }
+    //Do not comment out below this line method and class }}
 }
+
+
+
+
 
 
 
