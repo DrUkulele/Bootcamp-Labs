cffcdc58a6f9e09b2e234cfd4639178456d8b98d
DrUkulele <tlpriest@ymail.com>
2024-04-11 14:18:27 -0500

    adding to git hub test

---
+++ Workshops/FinancialCalculators/src/main/java/org/example/Calculators.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculators {
+    //mortgage calculator
+    public static float mortgage(){
+        System.out.println("This function has not been implemented!");
+        return 0;
+    }
+
+    //future value calculator
+    public static float futureValue(){
+        System.out.println("This function has not been implemented!");
+        return 0;
+    }
+
+    //calculate the present value of an ordinary annuity
+    public static float presentValue(){
+        System.out.println("This function has not been implemented!");
+        return 0;
+    }
+}
